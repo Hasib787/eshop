1cdd6fa91993b726d939a7a297afd4c1b7956b72
Md. Masud Rana <mr.hasib45@gmail.com>
2022-05-17 23:48:10 +0600

    [INIT]

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
-function App () {
+import React from 'react';
+function App() {
   return (
     <div>
       <h1>Hello Eshop</h1>
-      
     </div>
   );
-};
+}
 
 export default App;
